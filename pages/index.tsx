6862187d4b6d9f4139e85af6fccafff45fcdb907
Yassine El Houari <yas.elhouari@gmail.com>
2023-05-17 10:46:08 +0100

    update the index page to count for the new components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Text } from "@chakra-ui/react";
 import { useQuery } from "react-query";
 import { useMarvelShowQuery, useMarvelShowsQuery } from '@/api/api';
+import ShowList from '@/components/ShowList';
 
 const Home = () => {
-  const result = useMarvelShowQuery(1);
-  console.log('result: ', result);
-  return <Text>Edit /pages/index.tsx file</Text>;
+  const [loading, setLoading] = useState(true);
+  const [shows, setShows] = useState()
+  const {isLoading, data, error} = useMarvelShowsQuery();
+  useEffect(() => {
+    if(data) {
+      setShows(data);
+      setLoading(false);
+    }
+  }, [isLoading, data, error])
+  if(loading) {
+
+  return <Text>Loading...</Text>;
+  }
+  else return <ShowList shows={shows} />
 };
 
 export default Home;
-function getMarvelShows() {
-  throw new Error('Function not implemented.');
-}
 
