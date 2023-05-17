301ccb0a95bd5c0d36d1f1c0d1072465f400fae4
Yassine El Houari <yas.elhouari@gmail.com>
2023-05-17 02:51:15 +0100

    make the fetch for for both hooks

--- api/api.tsx
+++ api/api.tsx
@@ -2,19 +2,27 @@ import { useQuery } from 'react-query';
 import axios from 'axios';
 import { Movie, TvShow } from '../data';
 
-const getMarvelShows = async (): Promise<(Movie | TvShow)[]> => {
-  const { data } = await axios.get('/api/marvel/shows');
-  return data;
+const getMarvelShows = async () => {
+    try {
+    const res = await fetch("/api/marvel");
+    return res.json()
+    } catch(e) {
+      return e;
+    }
 };
 
 export const useMarvelShowsQuery = () => {
   return useQuery('marvelShows', getMarvelShows);
 };
 
-const getMarvelShow = async (id: number): Promise<Movie | TvShow> => {
-    const { data } = await axios.get(`/api/marvel/shows/${id}`);
-    return data;
-  };
+const getMarvelShow = async (id: number) => {
+  try {
+    const res = await fetch(`/api/marvel/${id}`);
+    return res.json();
+  } catch (e) {
+    return { error: 'Failed to fetch Marvel show' };
+  }
+};
   
 export const useMarvelShowQuery = (id: number) => {
   return useQuery(['marvelShow', id], () => getMarvelShow(id));
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import { Text } from "@chakra-ui/react";
 import { useQuery } from "react-query";
+import { useMarvelShowQuery, useMarvelShowsQuery } from '@/api/api';
 
 const Home = () => {
-  const getMarvel = async () => {
-    const res = await fetch("/api/marvel");
-    console.log({ res });
-    return res.json();
-  };
-  const { data } = useQuery("marvel", getMarvel);
-
-  console.log({ data });
+  const result = useMarvelShowQuery(1);
+  console.log('result: ', result);
   return <Text>Edit /pages/index.tsx file</Text>;
 };
 
 export default Home;
+function getMarvelShows() {
+  throw new Error('Function not implemented.');
+}
+
