eef40798e1b110a5b59f180975e1da4a00c1c281
Yassine El Houari <yas.elhouari@gmail.com>
2023-05-18 01:37:38 +0100

    make the entire Item clickable, not just the title

--- components/ShowItem.tsx
+++ components/ShowItem.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Box, Image, Text, LinkBox, LinkOverlay, Icon, Link } from '@chakra-ui/react';
+import { Box, Image, Text, Link, VStack, Icon } from '@chakra-ui/react';
 import { ExternalLinkIcon } from '@chakra-ui/icons';
 
 const ShowItem = ({ show }) => {
@@ -11,51 +11,51 @@ const ShowItem = ({ show }) => {
   };
 
   return (
-    <LinkBox as="article" maxW="sm" borderWidth="1px" borderRadius="md" overflow="hidden">
-      {show.cover_url && (
-        <Image src={imgSrc} alt={show.title} onError={handleError} height="400" objectFit="cover" />
-      )}
-      <Box p="6" bgColor="blackAlpha.800" data-testid="show-item">
-        <Box
-          mt={1}
-          fontWeight="semibold"
-          fontSize="lg"
-          lineHeight="tight"
-          color="white"
-          css={{
-            overflow: 'hidden',
-            textOverflow: 'ellipsis',
-            display: '-webkit-box',
-            WebkitLineClamp: 2,
-            WebkitBoxOrient: 'vertical',
-            height: 30,
-          }}
-        >
-          <Link href={`/show/${show.id}`} passHref>
-            <LinkOverlay>{show.title}</LinkOverlay>
-          </Link>
-        </Box>
-        <Text
-          mt={2}
-          color="whiteAlpha.700"
-          css={{
-            overflow: 'hidden',
-            textOverflow: 'ellipsis',
-            display: '-webkit-box',
-            WebkitLineClamp: 3,
-            WebkitBoxOrient: 'vertical',
-          }}
-        >
-          {show.overview}
-        </Text>
-        <Box mt={2}>
-          <Link href={`https://www.imdb.com/title/${show.imdb_id}`} color="red.400" isExternal mt="5">
-            <Icon as={ExternalLinkIcon} mr="2px" />
-            View on IMDB
-          </Link>
+    <Link href={`/show/${show.id}`} passHref>
+      <Box as="article" maxW="sm" borderWidth="1px" borderRadius="md" overflow="hidden" style={{ textDecoration: 'none' }}>
+        {show.cover_url && (
+          <Image src={imgSrc} alt={show.title} onError={handleError} height="400" objectFit="cover" />
+        )}
+        <Box p="6" bgColor="blackAlpha.800" data-testid="show-item">
+          <Box
+            mt={1}
+            fontWeight="semibold"
+            fontSize="lg"
+            lineHeight="tight"
+            color="white"
+            css={{
+              overflow: 'hidden',
+              textOverflow: 'ellipsis',
+              display: '-webkit-box',
+              WebkitLineClamp: 2,
+              WebkitBoxOrient: 'vertical',
+              height: 30,
+            }}
+          >
+            {show.title}
+          </Box>
+          <Text
+            mt={2}
+            color="whiteAlpha.700"
+            css={{
+              overflow: 'hidden',
+              textOverflow: 'ellipsis',
+              display: '-webkit-box',
+              WebkitLineClamp: 3,
+              WebkitBoxOrient: 'vertical',
+            }}
+          >
+            {show.overview}
+          </Text>
+          <Box mt={2}>
+            <Link href={`https://www.imdb.com/title/${show.imdb_id}`} color="red.400" isExternal mt="5">
+              <Icon as={ExternalLinkIcon} mr="2px" />
+              View on IMDB
+            </Link>
+          </Box>
         </Box>
       </Box>
-    </LinkBox>
+    </Link>
   );
 };
 
