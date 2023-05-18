2530d5df65da065489547ea3b82b57b09e6855aa
Yassine El Houari <yas.elhouari@gmail.com>
2023-05-18 01:00:29 +0100

    style the ShowItemDetails

--- components/ShowItemDetails.tsx
+++ components/ShowItemDetails.tsx
@@ -1,31 +1,41 @@
-import React from 'react';
-import { Box, Image, Text, Link } from "@chakra-ui/react";
+import React, { useState } from 'react';
+import { Box, Image, Text, Link, VStack, HStack } from "@chakra-ui/react";
 
 const ShowItemDetails = ({ item, loading }) => {
+  const [imgSrc, setImgSrc] = useState(item?.cover_url);
+  const defaultImage = 'https://upload.wikimedia.org/wikipedia/commons/d/d1/Image_not_available.png';
+
+  const handleError = () => {
+    setImgSrc(defaultImage);
+  };
   if(loading) {
     return 'Loading...';
   }
   return (
-    <Box border="1px" borderColor="gray.200" p="5" borderRadius="md" data-testid="show-item-details">
-      <Image src={item?.cover_url} alt={item.title} mb="5" />
-      <Text fontSize="xl" fontWeight="bold" mb="2">{item.title}</Text>
-      <Text mb="2">Release date: {item.release_date}</Text>
-      {item.type === "movie" ? (
-        <>
-          <Text mb="2">Box office: ${item.box_office}</Text>
-          <Text mb="2">Duration: {item.duration} min</Text>
-          <Text mb="2">Directed by: {item.directed_by}</Text>
-        </>
-      ) : (
-        <>
-          <Text mb="2">Last aired date: {item.last_aired_date}</Text>
-          <Text mb="2">Season: {item.season}</Text>
-          <Text mb="2">Number of episodes: {item.number_episodes}</Text>
-          <Text mb="2">Directed by: {item.directed_by}</Text>
-        </>
-      )}
-      <Text mb="2">Overview: {item.overview}</Text>
-      <Link href={`https://www.imdb.com/title/${item.imdb_id}`} isExternal>View on IMDB</Link>
+    <Box border="1px" borderColor="gray.200" p="5" borderRadius="md" bg="blackAlpha.900" color="white" data-testid="show-item-details">
+      <HStack spacing="24px">
+        <Image src={imgSrc} alt={item.title} mb="5" boxSize="300px" objectFit="cover" borderRadius="md" onError={handleError}/>
+        <VStack align="start" spacing="24px">
+          <Text fontSize="2xl" fontWeight="bold">{item.title}</Text>
+          <Text>Release date: {item.release_date}</Text>
+          {item.type === "movie" ? (
+            <>
+              <Text>Box office: ${item.box_office}</Text>
+              <Text>Duration: {item.duration} min</Text>
+              <Text>Directed by: {item.directed_by}</Text>
+            </>
+          ) : (
+            <>
+              <Text>Last aired date: {item.last_aired_date}</Text>
+              <Text>Season: {item.season}</Text>
+              <Text>Number of episodes: {item.number_episodes}</Text>
+              <Text>Directed by: {item.directed_by}</Text>
+            </>
+          )}
+        </VStack>
+      </HStack>
+      <Text mt="5" fontSize="lg">{item.overview}</Text>
+      <Link href={`https://www.imdb.com/title/${item.imdb_id}`} color="red.400" isExternal mt="5">View on IMDB</Link>
     </Box>
   );
 };
--- components/ShowList.tsx
+++ components/ShowList.tsx
@@ -7,7 +7,7 @@ const ShowList = ({ shows, loading }) => {
     return <Text>Loading...</Text>
   }
   return (
-    <Grid templateColumns="repeat(5, 1fr)" gap={6}>
+    <Grid templateColumns="repeat(5, 1fr)" gap={6} alignItems="stretch">
       {shows.map((show) => (
         <GridItem key={show.id}>
           <ShowItem show={show}/>
